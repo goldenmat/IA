0e44bf5b05928c4c03e606f51a32667876ee2644
chiesa <samuelechiesa.basta@gmail.com>
2017-03-16 16:14:04 +0100

    Update main.pl

--- main.pl
+++ main.pl
@@ -3,5 +3,20 @@
 :- use_module(library(strips)).
 :- include(library(declaration_syntax)).
 
-:- consult(mondi).
+:- writeln('******   COMANDI    *******'),
+   nl,
+   maplist(writeln, [
+	       '   genera_mondo(I).',
+	       '   genera un mondo con la dimensione inserita in I [I è un nat]',
+	       '   stampa_mondo',
+	       '   permette la visualizzazione del mondo',
+	       '   get_mondo(X).',
+	       '   restituisce il mondo casella per casella, X è una variabile',
+	       '   carica_mondo(<mondo>).',
+	       '   permette di caricare un mondo predefinito [mondo1, mondo2, mondo3]'
+	   ]),
+   nl, nl,
+   writeln('***************************'),
+   nl.
 
+:- consult(mondi).
